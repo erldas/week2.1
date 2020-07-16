37cfc662802e654937a0820c94b2706ebb3efa35
erldas <daserlikov@gmail.com>
2020-07-16 20:27:25 +0600

    Vasyaaa

---
+++ task44.py
@@ -0,0 +1,14 @@
+a =['Victor', 'Mike', 'Leo']
+a.insert(0, 'Vasya')
+a.insert(3, 'Sasha')
+a.append('Masha')
+
+
+print(a)
+b = 'Приходите ко мне в 22-00 без опозданий, '
+print(b+a[0])
+print(b+a[1])
+print(b+a[2])
+print(b+a[3])
+print(b+a[4])
+print(b+a[5])
